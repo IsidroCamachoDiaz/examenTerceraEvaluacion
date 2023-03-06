1eb5aef4e79c1622389d1f517ed2de4c2b6b22cb
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-03-06 10:00:34 +0100

    6-3-2023 v2

--- examenTerceraEvaluacion/src/main/webapp/script/gestionFechas.js
+++ examenTerceraEvaluacion/src/main/webapp/script/gestionFechas.js
@@ -2,8 +2,10 @@
 var ciudad= prompt("Introduca una ciudad de estas 3 Madrid,Paris o Londres");
 var fechaHoy = new Date();
 var ciudadElegida;
+
 //creo una variable para londres porque tiene una hora diferente y no imprima la de las otras 2
 var esCiudad=true;
+
 //Compruebo que no sea londres
 if(ciudad!="Londres"&&ciudad!="LONDRES"&&ciudad!="londres"){
 	
@@ -17,9 +19,10 @@ if(ciudad!="Londres"&&ciudad!="LONDRES"&&ciudad!="londres"){
 		esCiudad=false;
 		}
 }
+
 //si es londres
 else if(ciudad=="Londres"||ciudad=="LONDRES"||ciudad=="londres"){
-		//Cogo la hora de Inglaterra
+		//Cojo la hora de Inglaterra
 	let fechaopcion =fechaHoy.toLocaleString('en-GB');
 		//Presenta la fecha de londres
 	document.write("Ciudad: Londres <br>")
@@ -28,12 +31,16 @@ else if(ciudad=="Londres"||ciudad=="LONDRES"||ciudad=="londres"){
 	document.write("Hora: "+fechaopcion.substring(12, 20)+"<br>")
 	esCiudad=false;
 }
+
 //calcula hora para madrid y paris y la fecha
 var hora = fechaHoy.getHours() + ':' + fechaHoy.getMinutes() + ':' + fechaHoy.getSeconds();
 var fechaBien= fechaHoy.getDay()+"-"+(fechaHoy.getMonth()+1)+"-"+fechaHoy.getFullYear();
+
 //Presenta la fecha de paris y madrid
 if(esCiudad){
+	//Cojo la hora de España que es la misma que Inglaterra
+	let fechaopcion2 =fechaHoy.toLocaleString('es-ES');
 	document.write("Ciudad: "+ciudadElegida+"<br>")
-	document.write("Fecha: "+fechaBien+"<br>")
-	document.write("Hora: "+hora+"<br>")
+	document.write("Fecha: "+fechaopcion2.substring(0, 8)+"<br>")
+	document.write("Hora: "+fechaopcion2.substring(10, 20)+"<br>")
 }
